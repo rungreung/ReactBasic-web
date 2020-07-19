0313f98b3d0f9e8d6dd6e1d097e610e777508b51
unknown <rungreung.sak@gmail.com>
2020-07-19 14:21:56 +0700

    Updates

---
+++ precache-manifest.fe70eef2e73c6f3ec48b2b9465d5fbe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6fdfc019ad1cb5d1b7c244337d42e5",
+    "url": "/ReactBasic-web/index.html"
+  },
+  {
+    "revision": "a5ed63ab11774d83c76c",
+    "url": "/ReactBasic-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "01837071e780dbfc7848",
+    "url": "/ReactBasic-web/static/js/2.915d211a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactBasic-web/static/js/2.915d211a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ed63ab11774d83c76c",
+    "url": "/ReactBasic-web/static/js/main.619151e8.chunk.js"
+  },
+  {
+    "revision": "185ab3aae6d341fa2520",
+    "url": "/ReactBasic-web/static/js/runtime-main.0407e823.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactBasic-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
